7cc2ae7245dfc52de9e20f5f540887ec94b352fd
Samuel Tauil <samueltauil@gmail.com>
2016-12-08 14:44:34 -0500

    Update courseBase.sh

--- scenario/courseBase.sh
+++ scenario/courseBase.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #docker pull registry.access.redhat.com/openshift/origin:v1.3.1
-docker pull openshift/origin
+#docker pull openshift/origin
+
+docker run -d --name "origin" \
+        --privileged --pid=host --net=host \
+        -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys -v /var/lib/docker:/var/lib/docker:rw \
+        -v /var/lib/origin/openshift.local.volumes:/var/lib/origin/openshift.local.volumes \
+        openshift/origin start
