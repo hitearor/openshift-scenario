1e4fe84eda36122e25841828641d218560765977
Samuel Tauil <samueltauil@gmail.com>
2016-12-08 15:56:54 -0500

    Update courseBase.sh

--- scenario/courseBase.sh
+++ scenario/courseBase.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-#docker pull registry.access.redhat.com/openshift/origin:v1.3.1
-#docker pull openshift/origin
-
-oc adm registry
+ssh -i ~/.ssh/id_rsa root@host01 "/root/.set-hostname && sed -i 's/\[\[KATACODA_URL\]\]/https:\/\/[[HOST_SUBDOMAIN]]-8443-[[KATACODA_HOST]].environments.katacoda.com:443/g' /etc/systemd/system/origin.service && rm -rf /openshift.local* && systemctl daemon-reload && systemctl restart origin"
+ssh -i ~/.ssh/id_rsa root@host01 "curl -ksLO https://raw.githubusercontent.com/openshift/origin/master/examples/hello-openshift/hello-pod.json"
